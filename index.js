1dc70b5cd0324cc4ca5863bd5523127dec5306cf
Ritik Srivastava <ritik.space@gmail.com>
2021-11-18 19:58:06 +0530

    Remove Logger

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require('express'),
 	chalk = require('chalk'),
 	app = express(),
-	request = require('request');
-
-	initLogger = require('./utils/logger'),
+	request = require('request'),
 	{ getAllCountries, getCountryCodeByName, getCountryCodeByDialCode, getCountryFlagByCountryCode,
 		getCountryFlagByName, getCountryFlagByDialCode } = require('./utils/getCountries');
 
 require('dotenv');
-initLogger();
 
 app.get('/getAllCountries', (req, res) => {
 	res.json(getAllCountries());
@@ -27,6 +24,6 @@ app.get('/getCountryFlagByName', (req, res) => {
 });
 
 app.listen(process.env.PORT || 8080, () => {
-	console.log(true, 'Starting Server');
-	console.log('✔️', 'Server is running at', chalk.green('http://' + (process.env.IP || 'localhost') + ':' + (process.env.PORT || 8080)));
+	console.log('⪢   ', 'Starting Server');
+	console.log('✔️   ', 'Server is running at', chalk.green('http://' + (process.env.IP || 'localhost') + ':' + (process.env.PORT || 8080)));
 });
